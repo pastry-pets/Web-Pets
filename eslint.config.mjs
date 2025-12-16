9bf855be7a12961f848b6224223c74d124572499
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2025-12-15 18:44:04 -0600

    add rules to require semicolons and forbid console.logs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, rules: {semi: ["error", "always"], "no-console": ["error", {allow: ["warn", "info", "error"]}]} },
   pluginReact.configs.flat.recommended,
 ]);
