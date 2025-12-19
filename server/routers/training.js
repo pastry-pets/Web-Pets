360dfcdce8f88c9584ead566fbffc657e893f6cf
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2025-12-18 18:33:43 -0600

    send 401 if unauthenticated and send training data for GET /training

--- server/routers/training.js
+++ server/routers/training.js
@@ -1,24 +1,54 @@
 const express = require('express');
+const { Pet } = require('../db');
 
 const router = express.Router();
 
 // GET all skill data for the pet belonging to the current user
 router.get('/', (req, res) => {
-  const userId = req.session.passport?.user.id;
-  res.send('ultimately, this should return all of the pet\'s skill levels');
+  const userId = req.session.passport?.user?.id;
+
+  if (userId === undefined) {
+    res.sendStatus(401);
+    return;
+  }
+
+  return Pet.find({ userId })
+    .then((pets) => {
+      if (!pets.length) {
+        res.sendStatus(404);
+        return;
+      }
+      res.status(200).send(pets[0].training);
+    })
+    .catch((err) => {
+      console.error('Failed to retrieve pet data:', err);
+      res.sendStatus(500);
+    });
 });
 
 // GET data for the specified skill for the pet belonging to the current user
 router.get('/:id', (req, res) => {
+  const userId = req.session.passport?.user?.id;
+
+  if (userId === undefined) {
+    res.sendStatus(401);
+    return;
+  }
+
   const skillId = req.params.id;
-  const userId = req.session.passport?.user.id;
   res.send('get id');
 });
 
 // PATCH data to update the specified skill by the delta amount for the pet belonging to the current user
 router.patch('/:id', (req, res)=> {
+  const userId = req.session.passport?.user?.id;
+
+  if (userId === undefined) {
+    res.sendStatus(401);
+    return;
+  }
+
   const skillId = req.params.id;
-  const userId = req.session.passport?.user.id;
 
   const skillDelta = req.body.delta;
 
