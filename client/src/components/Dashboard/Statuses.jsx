0187d6bc978dbfdaca49a1f645ad9c6dab47743e
azaria <azariaplayer@gmail.com>
2026-01-02 19:53:11 -0600

    Added documentation for the Statuses component.

--- client/src/components/Dashboard/Statuses.jsx
+++ client/src/components/Dashboard/Statuses.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-
+/**
+ * @module Statuses
+ * @description
+ * This component will render the status of the pet. This will display the
+ * mood, love, hunger, and health. It gets this information from the database
+ * based on user that is signed in.
+ */
 const Status = ({pet}) => {
 
   const { mood, love, health, hunger } = pet;
