c6fa01fa1eb47cc58707bc3081ba26b447546984
cfields05 <senken1218@gmail.com>
2026-01-05 09:54:35 -0600

    DashboardView documentation

--- client/src/components/Dashboard/DashboardView.jsx
+++ client/src/components/Dashboard/DashboardView.jsx
@@ -6,10 +6,13 @@ import Statuses from './Statuses';
 import Interactions from './Interactions';
 
 const DashboardView = ({ pet, user, availableSkills, behaviors, behaviorMessage, displayMessage, refreshSkillData, refreshPet }) => {
-  
+  /**
+   * A state variable determines which tab in the dashboard should be rendered. Selected by clicking the tab buttons location in the dashboard.
+   * @type {string}
+   * @name tab
+  */
   const [ tab, setTab ] = useState('Interactions');
 
-  // const { pet, user } = props;
   const tabs = ['Interactions', 'Skills'];
 
   // original: style={{ border: '1px solid black', marginTop: '5px' }}
@@ -30,6 +33,11 @@ const DashboardView = ({ pet, user, availableSkills, behaviors, behaviorMessage,
 
   const tabSelectedStyles = tabStyles.concat('border-b-device');
 
+  /**
+   * Handles the rendering logic for the dashboard tabs, and what to display if the user is not logged in or has not pet.
+   * @name renderTab
+   * @function
+   */
   const renderTab = () => {
     if (pet) {
       switch (tab) {
@@ -54,18 +62,27 @@ const DashboardView = ({ pet, user, availableSkills, behaviors, behaviorMessage,
     }
   };
 
+  /**
+   * A simple function to decide if the pet statuses should be able to be rendered.
+   * @name onlyStatus
+   * @function
+   */
   const onlyStatus = () => {
     if(pet) {
       return <Statuses pet={pet} />;
     }
   };
 
-
+  /**
+   * A simple function to set which tab to display upon clicking the corresponding button.
+   * @name handleTabSelect
+   * @function
+   * @param {event} event - the event fired when clicking a dashboard tab button, its target value holds the name of the tab ('Interactions' or 'Skills')
+   */
   const handleTabSelect = (event) => {
     setTab(event.target.name);
   };
 
-  // TODO: display "off" dashboard when logged out instead of displaying nothing
   return (
     <div className={dashBoardStyles.join(' ')}>
       <div className="border-2 border-solid" >
